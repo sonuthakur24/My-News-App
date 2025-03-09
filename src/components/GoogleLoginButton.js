5fe47c88b69a44cf602a61428e1201c772e6b253
Sonu Thakur <113580850+sonuthakur24@users.noreply.github.com>
2025-03-09 12:52:38 +0530

    Update GoogleLoginButton.js

--- src/components/GoogleLoginButton.js
+++ src/components/GoogleLoginButton.js
@@ -4,11 +4,12 @@ import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 export default function GoogleLoginButton() {
   const { data: session } = useSession();
+  const callbackUrl = process.env.NEXT_PUBLIC_CALLBACK_URL || "https://my-news-app-zeta.vercel.app";
 
   if (!session) {
     return (
       <button
-        onClick={() => signIn("google", { callbackUrl: "http://localhost:3000" })}
+        onClick={() => signIn("google", { callbackUrl })}
         className="w-full flex items-center justify-center bg-red-500 text-white p-2 rounded mt-4 hover:bg-red-600 transition duration-300"
       >
         <FontAwesomeIcon icon={faGoogle} className="mr-2" />
@@ -21,7 +22,7 @@ export default function GoogleLoginButton() {
     <div className="mt-4 text-center">
       <p className="text-gray-700 mb-2">You are logged in as {session.user.email}!</p>
       <button
-        onClick={() => signOut({ callbackUrl: "http://localhost:3000" })}
+        onClick={() => signOut({ callbackUrl })}
         className="w-full bg-red-500 text-white p-2 rounded hover:bg-red-600 transition duration-300"
       >
         Sign out
